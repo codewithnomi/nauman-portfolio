6145f3772439fc2e26f30094014a839954d16506
codewithnomi <43666965+codewithnomi@users.noreply.github.com>
2025-09-18 15:11:03 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -236,6 +236,7 @@ const Contact = () => {
   netlify-honeypot="bot-field"
   onSubmit={handleSubmit}
   className="space-y-6"
+  netlify
 >
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <div>
